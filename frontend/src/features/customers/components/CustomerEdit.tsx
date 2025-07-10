fc066f6a71679e364d31f01101a3e988bf8c5155
p-febis <preston.febis@yahoo.com>
2025-07-10 00:38:37 -0300

    Moved the customer edit form to a card

--- frontend/src/features/customers/components/CustomerEdit.tsx
+++ frontend/src/features/customers/components/CustomerEdit.tsx
@@ -8,6 +8,15 @@ import { toast } from "sonner";
 import { useNavigate } from "react-router";
 import { DeleteButton } from "@/components/DeleteButton";
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 export const CustomerEdit = ({ customer }: { customer: Customer }) => {
   const { id, ...restCustomer } = customer;
 
@@ -40,66 +49,73 @@ export const CustomerEdit = ({ customer }: { customer: Customer }) => {
 
   return (
     <div className="p-4 flex items-center justify-center h-full">
-      <form
-        className="w-sm md:min-w-md space-y-4"
-        onSubmit={(event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <form.Field
-          name="name"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Name:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <form.Field
-          name="email"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Email:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <form.Field
-          name="phone"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Phone number:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <div className="w-full inline-flex justify-between">
-          <Button type="submit" disabled={isDefaultValue}>
-            Submit
-          </Button>
-          <DeleteButton resource="customers" id={String(id)} />
-        </div>
-      </form>
+    <Card>
+      <CardHeader>
+	<CardTitle>Create a customer</CardTitle>
+      </CardHeader>
+      <CardContent>
+	  <form
+	    className="w-sm md:min-w-md space-y-4"
+	    onSubmit={(event) => {
+	      event.preventDefault();
+	      event.stopPropagation();
+	      form.handleSubmit();
+	    }}
+	  >
+	    <form.Field
+	      name="name"
+	      children={(field) => (
+		<>
+		  <label htmlFor={field.name}>Name:</label>
+		  <Input
+		    id={field.name}
+		    name={field.name}
+		    value={field.state.value}
+		    onBlur={field.handleBlur}
+		    onChange={(event) => field.handleChange(event.target.value)}
+		  />
+		</>
+	      )}
+	    />
+	    <form.Field
+	      name="email"
+	      children={(field) => (
+		<>
+		  <label htmlFor={field.name}>Email:</label>
+		  <Input
+		    id={field.name}
+		    name={field.name}
+		    value={field.state.value}
+		    onBlur={field.handleBlur}
+		    onChange={(event) => field.handleChange(event.target.value)}
+		  />
+		</>
+	      )}
+	    />
+	    <form.Field
+	      name="phone"
+	      children={(field) => (
+		<>
+		  <label htmlFor={field.name}>Phone number:</label>
+		  <Input
+		    id={field.name}
+		    name={field.name}
+		    value={field.state.value}
+		    onBlur={field.handleBlur}
+		    onChange={(event) => field.handleChange(event.target.value)}
+		  />
+		</>
+	      )}
+	    />
+	    <div className="w-full inline-flex justify-between">
+	      <Button type="submit" disabled={isDefaultValue}>
+		Submit
+	      </Button>
+	      <DeleteButton resource="customers" id={String(id)} />
+	    </div>
+	  </form>
+      </CardContent>
+    </Card>
     </div>
   );
 };
