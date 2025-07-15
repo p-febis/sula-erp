f75fc20867abe31baebf4e291c803cb21ddab437
p-febis <preston.febis@yahoo.com>
2025-07-15 10:10:58 -0300

    frontend: improve ui views

--- frontend/src/features/authentication/components/RoleCreate.tsx
+++ frontend/src/features/authentication/components/RoleCreate.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { useCreateRole } from "../hooks/use-create-role";
 import { toast } from "sonner";
 import { useNavigate } from "react-router";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const RoleCreate = () => {
   const navigate = useNavigate();
@@ -32,33 +33,40 @@ export const RoleCreate = () => {
 
   return (
     <div className="p-4 flex items-center justify-center h-full">
-      <form
-        className="w-sm md:min-w-md space-y-4"
-        onSubmit={(event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <form.Field
-          name="name"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Name:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <Button type="submit" disabled={isDefaultValue}>
-          Submit
-        </Button>
-      </form>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create role</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            className="w-sm md:min-w-md space-y-4"
+            onSubmit={(event) => {
+              event.preventDefault();
+              event.stopPropagation();
+              form.handleSubmit();
+            }}
+          >
+            <form.Field
+              name="name"
+              children={(field) => (
+                <>
+                  <label htmlFor={field.name}>Name:</label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(event) => field.handleChange(event.target.value)}
+                  />
+                </>
+              )}
+            />
+            <Button type="submit" disabled={isDefaultValue}>
+              Submit
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
--- frontend/src/features/customers/components/CustomerCreate.tsx
+++ frontend/src/features/customers/components/CustomerCreate.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { toast } from "sonner";
 import { useNavigate } from "react-router";
 import { useCreateCustomer } from "../hooks/use-create-customer";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export const CustomerCreate = () => {
   const navigate = useNavigate();
@@ -34,63 +35,70 @@ export const CustomerCreate = () => {
 
   return (
     <div className="p-4 flex items-center justify-center h-full">
-      <form
-        className="w-sm md:min-w-md space-y-4"
-        onSubmit={(event) => {
-          event.preventDefault();
-          event.stopPropagation();
-          form.handleSubmit();
-        }}
-      >
-        <form.Field
-          name="name"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Name:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <form.Field
-          name="email"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Email:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <form.Field
-          name="phone"
-          children={(field) => (
-            <>
-              <label htmlFor={field.name}>Phone number:</label>
-              <Input
-                id={field.name}
-                name={field.name}
-                value={field.state.value}
-                onBlur={field.handleBlur}
-                onChange={(event) => field.handleChange(event.target.value)}
-              />
-            </>
-          )}
-        />
-        <Button type="submit" disabled={isDefaultValue}>
-          Submit
-        </Button>
-      </form>
+      <Card>
+        <CardHeader>
+          <CardTitle>Create customer</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form
+            className="w-sm md:min-w-md space-y-4"
+            onSubmit={(event) => {
+              event.preventDefault();
+              event.stopPropagation();
+              form.handleSubmit();
+            }}
+          >
+            <form.Field
+              name="name"
+              children={(field) => (
+                <>
+                  <label htmlFor={field.name}>Name:</label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(event) => field.handleChange(event.target.value)}
+                  />
+                </>
+              )}
+            />
+            <form.Field
+              name="email"
+              children={(field) => (
+                <>
+                  <label htmlFor={field.name}>Email:</label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(event) => field.handleChange(event.target.value)}
+                  />
+                </>
+              )}
+            />
+            <form.Field
+              name="phone"
+              children={(field) => (
+                <>
+                  <label htmlFor={field.name}>Phone number:</label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(event) => field.handleChange(event.target.value)}
+                  />
+                </>
+              )}
+            />
+            <Button type="submit" disabled={isDefaultValue}>
+              Submit
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
     </div>
   );
 };
