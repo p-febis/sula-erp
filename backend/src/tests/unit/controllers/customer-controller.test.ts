80d9890f31738feda16c02f9bba2acfe7df96dc0
p-febis <preston.febis@yahoo.com>
2025-07-17 10:33:10 -0300

    backend: Formatting

--- backend/src/tests/unit/controllers/customer-controller.test.ts
+++ backend/src/tests/unit/controllers/customer-controller.test.ts
@@ -279,19 +279,20 @@ describe("CustomerController", () => {
     mockCustomerService.updateCustomer.mockResolvedValueOnce(sampleCustomer);
     mockAuthorizationService.userCanDo.mockReturnValueOnce(true);
 
-    const event = new H3Event(createRequest({ method: "PATCH", body: invalidBody }));
+    const event = new H3Event(
+      createRequest({ method: "PATCH", body: invalidBody }),
+    );
     event.context.params = { id: "1" };
     event.context.claims = baseClaims;
 
-    const error = await controller.updateOne(event).catch(e => e);
+    const error = await controller.updateOne(event).catch((e) => e);
 
     expect(error.cause).toEqual({
       status: 400,
       statusText: "Bad Request",
       message: "Bad Request",
       data: null,
-    })
-
+    });
   });
 
   it("should throw when user does not have permission to update a customer", async () => {
